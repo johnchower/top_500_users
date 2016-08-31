5384041de142ebb137b3adbc9e5f589f351cf3a4
johnchower <johnchower@gmail.com>
2016-08-31 16:57:19 -0600

    Add a block of code that returns the top users as a list, instead of separate variables.

--- find_top_users.R
+++ find_top_users.R
@@ -1,16 +1,16 @@
 # find_top_users
 
-top_user_list <- user_tier_cutoffs %>%
-  {setNames(., paste("top", ., "users", sep = "_"))} %>%
-  as.list %>%
-  llply(
-    .fun = find_top_n_users
-  )
+# top_user_list <- user_tier_cutoffs %>%
+#   {setNames(., paste("top", ., "users", sep = "_"))} %>%
+#   as.list %>%
+#   llply(
+#     .fun = find_top_n_users
+#   )
 
-# for(N in user_tier_cutoffs){
-#   name <- paste("top", N, "users", sep = "_")
-#   
-#   find_top_n_users(n = N) %>% {
-#     assign(name, ., envir = globalenv())
-#   }
-# }
+for(N in user_tier_cutoffs){
+  name <- paste("top", N, "users", sep = "_")
+
+  find_top_n_users(n = N) %>% {
+    assign(name, ., envir = globalenv())
+  }
+}
--- fn_find_top_n_users.R
+++ fn_find_top_n_users.R
@@ -9,10 +9,10 @@ find_top_n_users <- function(n,
     group_by(user_id) %>% 
     summarise(number_of_active_days_past28 = length(unique(date_id)),
               number_of_actions_past_28 = n()) %>% {
-      if(results_by == "actions"){
-        return(arrange(., desc(number_of_actions_past_28)))
-      } else {return(arrange(., desc(number_of_active_days_past_28)))}           
-    } %>%
+                if(results_by == "actions"){
+                  return(arrange(., desc(number_of_actions_past_28)))
+                } else {return(arrange(., desc(number_of_active_days_past_28)))}           
+              } %>%
     slice(1:n) %>%
     {.$user_id} %>%
     unique
