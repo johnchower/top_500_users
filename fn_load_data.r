e3a1bf281f6cb51b196a82bc0cc31a95e40d7f84
johnchower <johnchower@gmail.com>
2016-09-06 18:45:47 -0700

    Create a function to handle the load-data step

---
+++ fn_load_data.r
@@ -0,0 +1,21 @@
+# Function: load_data
+# Reads all csv files in looker.csvs and static.csvs
+# Outputs a list of data frames for further manipulation.
+
+load_data <- function(name_list,
+                      looker.csvs = paste(find_root(has_file("README.md")),
+                                          "looker_csvs",
+                                          sep = "/"),
+                      static.csvs = paste(find_root(has_file("README.md")),
+                                          "static_csvs", 
+                                          sep = "/")){
+x <- looker.csvs %>%
+  {paste(., dir(.), sep = "/")} %>%
+  lapply(FUN = function(path)read.csv(path, stringsAsFactors = F)) 
+
+y <- static.csvs %>%
+  {paste(., dir(.), sep = "/")} %>%
+  lapply(FUN = function(path)read.csv(path, stringsAsFactors = F))
+
+c(x,y)
+}
