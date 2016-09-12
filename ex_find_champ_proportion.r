2f36454841414b72a7b34ee7e088dd9c7a619912
johnchower <johnchower@gmail.com>
2016-09-12 10:27:42 -0600

    Add file to find champion proportions

---
+++ ex_find_champ_proportion.r
@@ -0,0 +1,8 @@
+# Exploration: find_champ_proportion
+
+# Find proportion of users in a given subset who have taken a "champion only"
+# action at some point.
+
+find_champ_proportion <- function(user_subset,
+                                  ,
+
