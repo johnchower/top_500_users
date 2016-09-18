ab181dfe21904175c226e78eed0d8065a97fb159
johnchower <johnchower@gmail.com>
2016-09-18 14:56:21 -0600

    Fix notification script.

--- ex_notifications.R
+++ ex_notifications.R
@@ -1,9 +1,12 @@
-notification_status <- dbGetQuery(con,
-  "
-  select user_id, status,
+notification_status <- dbGetQuery(con,"
+  select user_id,
+    status,
     count(*)
   from notification_events
   group by user_id, status
-  order by user_id, status;
-  "
-)
+  order by user_id, status
+  ;") %>%
+  group_by(user_id) %>%
+  mutate(total_notifications = sum(count),
+         percent = count/total_notifications) %>%
+  arrange(user_id) 
