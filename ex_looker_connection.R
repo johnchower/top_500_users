cde1246459f3a6b56523ade6ffa345f8272d64a4
johnchower <johnchower@gmail.com>
2016-09-19 11:11:38 -0600

    Learn how to use inline Looker queries.

--- ex_looker_connection.R
+++ ex_looker_connection.R
@@ -2,5 +2,14 @@ library(LookR)
 looker_setup(id = "mz8bPyHzY7MY5JNhSmcj",
              secret = "vttZrNKZGcCyB8xyGq68qfVR",
              api_path = "https://api-looker.gloo.us/api/3.0")
-look_1205 <- run_look(look_id =2160)
-print(nrow(look_1205))
+
+user_connected_to_champion <- run_inline_query(model = "gloo",
+                 view = "user_platform_action_facts",
+                 fields = c("user_dimensions.id",
+                          "user_connected_to_champion_dimensions.id"),
+                 filters = list(c("user_dimensions.id", "24"))) %>%
+  rename(user_id = user_dimensions.id,
+         connected_to_champion = user_connected_to_champion_dimensions.id) %>%
+  melt(id.vars = "user_id")
+head(query_1)
+
